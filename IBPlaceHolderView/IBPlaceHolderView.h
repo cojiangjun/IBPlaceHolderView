2ec004fa3f044cd77f3276e269b4c24a56225a3f
gfwangfei <gfwangfei@oa.gf.com.cn>
2016-02-19 11:16:48 +0800

    Initial Commit

---
+++ IBPlaceHolderView/IBPlaceHolderView.h
@@ -0,0 +1,19 @@
+//
+//  IBPlaceHolderView.h
+//  IBPlaceHolderView
+//
+//  Created by rcio on 16/2/19.
+//  Copyright © 2016年 GF Securities. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IBPlaceHolderView.
+FOUNDATION_EXPORT double IBPlaceHolderViewVersionNumber;
+
+//! Project version string for IBPlaceHolderView.
+FOUNDATION_EXPORT const unsigned char IBPlaceHolderViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IBPlaceHolderView/PublicHeader.h>
+
+
